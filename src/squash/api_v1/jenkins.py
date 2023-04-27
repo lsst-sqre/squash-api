c524bcf3e71ff8997784364dea1ef0408d4291b6
Angelo Fausti <angelofausti@gmail.com>
2023-04-27 16:21:14 -0700

    Fix up URL arg parse

--- src/squash/api_v1/jenkins.py
+++ src/squash/api_v1/jenkins.py
@@ -5,12 +5,6 @@ from ..models import EnvModel, JobModel
 
 class Jenkins(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument(
-        "ci_name",
-        type=str,
-        required=True,
-        help="This field cannot be left blank.",
-    )
 
     def get(self, ci_id):
         """
@@ -30,14 +24,10 @@ class Jenkins(Resource):
           404:
             description: Jenkins job not found.
         """
-        args = self.parser.parse_args()
-        ci_name = args["ci_name"]
         env = EnvModel.find_by_name(env_name="jenkins")
 
         if env:
-            job = JobModel.find_by_env_data(
-                env_id=env.id, ci_id=ci_id, ci_name=ci_name
-            )
+            job = JobModel.find_by_env_data(env_id=env.id, ci_id=ci_id)
         else:
             message = "Environment `jenkins` not found."
             return {"message": message}, 400
